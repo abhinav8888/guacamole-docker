744e06b3e1115ad3fe46557facc4134d1d2d9a81
Kumar Abhinav <abhinav9011032@gmail.com>
2019-08-09 02:38:06 +0530

    Fixed error in docker restart script

--- container-files/enable-docker-restart.sh
+++ container-files/enable-docker-restart.sh
@@ -1,12 +1,9 @@
 sudo systemctl enable docker
 
 if [ -f container-files/containers-restart.service ]; then
-    sudo mv container-files/conainers-restart.service /etc/systemd/system/containers-restart.service
-    echo "file moved"
-    sudo ls -lah /etc/systemd/system/
+    sudo mv container-files/containers-restart.service /etc/systemd/system/containers-restart.service
 
 fi
 sudo systemctl enable containers-restart
-echo "DONE"
 
 
